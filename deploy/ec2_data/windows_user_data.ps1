35ecd8532de0e3c7811cb5c91e11448e33686e65
dmtgrinevich <dgrinevich@solvd.com>
2024-08-16 14:39:57 +0000

    fixed user_data var

--- deploy/ec2_data/windows_user_data.ps1
+++ deploy/ec2_data/windows_user_data.ps1
@@ -9,7 +9,7 @@
     [Environment]::SetEnvironmentVariable("ECS_ENABLE_CONTAINER_METADATA", "$TRUE", "Machine")
     [Environment]::SetEnvironmentVariable("ECS_IMAGE_PULL_BEHAVIOR", "prefer-cached", "Machine")
     
-    Initialize-ECSAgent -Cluster '${cluster_name}' -EnableTaskIAMRole -AwsvpcBlockIMDS -LoggingDrivers '["json-file","awslogs"]' -EnableTaskIAMRole -AwsvpcAdditionalLocalRoutes '["{cidr_block}"]'
+    Initialize-ECSAgent -Cluster '${cluster_name}' -EnableTaskIAMRole -AwsvpcBlockIMDS -LoggingDrivers '["json-file","awslogs"]' -EnableTaskENI -AwsvpcAdditionalLocalRoutes '["${cidr_block}"]'
 
     Set-TimeZone -Name "Pacific Standard Time"
 </powershell>
