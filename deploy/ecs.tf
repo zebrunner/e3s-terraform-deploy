4a247ebcbab09a4f9d02cf38cdd729e782307f4e
dmtgrinevich <dgrinevich@solvd.com>
2024-10-04 12:01:01 +0000

    Deleted task def tag

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -62,10 +62,6 @@ resource "aws_ecs_task_definition" "linux_node_exporter" {
   task_role_arn            = aws_iam_role.e3s_exporter[0].arn
   network_mode             = "host"
 
-  tags = {
-    "aws:ecs:serviceManaged" = ""
-  }
-
   volume {
     name      = "proc"
     host_path = "/proc"
