b66bdd8fc30251af7c66e69a548900a56ef72127
Vadym Spitsyn <vspitsyn@solvd.com>
2025-12-02 13:37:13 +0100

    Add AMIs info to sns msg

--- deploy/sns.tf
+++ deploy/sns.tf
@@ -63,3 +63,18 @@ resource "aws_secretsmanager_secret_version" "sns_topics" {
     FAILURE_SNS_TOPIC_ARN = aws_sns_topic.failure[0].arn
   })
 }
+
+# Local File with AMI Information
+########################################################################################################################
+
+resource "local_file" "ami_info" {
+  filename = "${path.module}/ami_info.txt"
+  content = jsonencode({
+    e3s_server_ami_id      = data.aws_ami.ubuntu_22_04.id
+    e3s_server_ami_name    = data.aws_ami.ubuntu_22_04.name
+    e3s_server_ami_date    = data.aws_ami.ubuntu_22_04.creation_date
+    esg_worker_node_ami_id       = data.aws_ami.zbr_linux.id
+    esg_worker_node_ami_name     = data.aws_ami.zbr_linux.name
+    esg_worker_node_ami_date     = data.aws_ami.zbr_linux.creation_date
+  })
+}
