b60562cd1cea2502e569736b20d5909d21ce61c7
Vadym Spitsyn <vspitsyn@solvd.com>
2025-12-08 11:12:06 +0100

    Add ami data writing to local file

--- deploy/iam.tf
+++ deploy/iam.tf
@@ -197,3 +197,15 @@ resource "aws_iam_role_policy" "event_bridge" {
     }
   )
 }
+
+resource "local_file" "ami_info" {
+  filename = "${path.module}/ami_info.txt"
+  content = jsonencode({
+    esg_server_ami_id      = data.aws_ami.ubuntu_22_04.id
+    esg_server_ami_name    = data.aws_ami.ubuntu_22_04.name
+    esg_server_ami_date    = data.aws_ami.ubuntu_22_04.creation_date
+    esg_worker_node_ami_id       = data.aws_ami.zbr_linux.id
+    esg_worker_node_ami_name     = data.aws_ami.zbr_linux.name
+    esg_worker_node_ami_date     = data.aws_ami.zbr_linux.creation_date
+  })
+}
